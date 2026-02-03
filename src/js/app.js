20fa77f30ed5aac67af02e06a2bcf4d0a4afd986
FSobrino92 <141543049+FSobrino92@users.noreply.github.com>
2026-02-03 14:06:28 +0100

    Performance Web

--- src/js/app.js
+++ src/js/app.js
@@ -25,9 +25,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = "Imagen galeria"
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+             <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+             <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+             <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
 
         //Event Handler
         imagen.onclick = function () {
@@ -39,10 +42,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = "Imagen galeria"
-
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
 
     // Generar modal
     const modal = document.createElement('DIV')
@@ -99,7 +104,7 @@ function resaltarEnlace() {
 
         navLinks.forEach(link => {
             link.classList.remove('active')
-            if(link.getAttribute('href') === '#' + actual) {
+            if (link.getAttribute('href') === '#' + actual) {
                 link.classList.add('active')
             }
         });
